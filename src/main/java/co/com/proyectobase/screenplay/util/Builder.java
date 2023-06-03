04a212d5c527d4316858819559c609865142ae7f
Luis Carlos Marin Campos <luiscarlosmarca@gmail.com>
2023-06-03 07:19:46 -0500

    Delete src directory

--- src/main/java/co/com/proyectobase/screenplay/util/Builder.java
+++
@@ -1,5 +0,0 @@
-package co.com.proyectobase.screenplay.util;
-
-public interface Builder<T> {
-    T build();
-}
--- src/main/java/co/com/proyectobase/screenplay/util/Constantes.java
+++
@@ -1,7 +0,0 @@
-package co.com.proyectobase.screenplay.util;
-
-public class Constantes {
-    public static final String NAVEGADOR = "chrome";
-    public static final Integer POSICION_REGISTRO = 0;
-    public static final String DIA_REPORTADO = "diaReportado";
-}
